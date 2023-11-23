63460f04542bbffd7fc864077196dada1d0fc2dc
MarynaKindras <maryna.kindras@gmail.com>
2023-11-23 17:05:12 +0200

    Main 2

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
-  'live_ 5LNqLi7hA220b3is0eAXsr4fXkIdpB zuUhUqHdIMTxsYbLuejzcODq9K4Nf7';
+  'live_5LNqLi7hA220b3is0eAXsr4fXkIdpBzuUhUqHdIMTxsYbLuejzcODq9K4Nf7sk47';
 
 export async function fetchBreeds() {
   const response = await axios.get('https://api.thecatapi.com/v1/breeds');
